9327da86bcb7afdd5f6962287e04d2a4060a9c4c
xjl;gvho;idfsjhlkfdsjhlk <thomas.colomba30@gmail.com>
2019-07-10 22:37:47 +0200

    first commit

--- README.md
+++ README.md
@@ -1,29 +1,101 @@
-# ejemplo16
+# Bind html inputs elements to a variable of a component
+For those html elements :
+<br/>
+input text
+<br/>
+input checkbox
+<br/>
+input date
+<br/>
+input radio
+<br/>
+select single
+<br/>
+select multiple
+<br/>
+<br/>
 
-## Project setup
-```
-npm install
-```
+input text :
+<br/>
+html&nbsp;=>&nbsp;&nbsp;&nbsp;``<input type="text" v-model="myText"/>``
+<br/>
+js&nbsp;&nbsp;&nbsp;=>&nbsp;&nbsp;&nbsp;myText: ''
+<br/>
+<br/>
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+input checkbox :
+<br/>
+html =>&nbsp;&nbsp;&nbsp;``<input type="text" v-model="myCheckbox"/>``
+<br/>
+js&nbsp;&nbsp;&nbsp;=>&nbsp;&nbsp;&nbsp;myCheckbox: false
+<br/>
+<br/>
 
-### Compiles and minifies for production
-```
-npm run build
-```
+input date :
+<br/>
+html =>&nbsp;&nbsp;&nbsp;``<input type="date" v-model="myDate"/>``
+<br/>
+js&nbsp;&nbsp;&nbsp;=>&nbsp;&nbsp;&nbsp;myDate: null
+<br/>
+<br/>
 
-### Run your tests
-```
-npm run test
-```
+input radio :
+<br/>
+html =>
+<br/>
+``<input type="radio" name="myRBGroup" id="rb1" v-model="myRB" value="RB1"/>``
+<br/>
+``<input type="radio" name="myRBGroup" id="rb2" v-model="myRB" value="RB2"//>``
+<br/>
+js&nbsp;&nbsp;&nbsp;=>&nbsp;&nbsp;&nbsp;myRB: null
+<br/>
+<br/>
 
-### Lints and fixes files
-```
-npm run lint
-```
+single select :
+<br/>
+html =>
+<br/>
+``<select v-model="selectSingle">``
+<br/>
+``  <option value="option1_1">option1_1</option>``
+<br/>
+``  <option value="option1_2">option1_2</option>``
+<br/>
+``  <option value="option1_3">option1_3</option>``
+<br/>
+``  <option value="option1_4">option1_4</option>``
+<br/>
+``</select>``
+<br/>
+js&nbsp;&nbsp;&nbsp;=>&nbsp;&nbsp;&nbsp;selectSingle: null,
+<br/>
+<br/>
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+multiple select :
+<br/>
+html =>
+<br/>
+``<select multiple v-model="selectMultiple">``
+<br/>
+``  <option value="option2_1">option2_1</option>``
+<br/>
+``  <option value="option2_2">option2_2</option>``
+<br/>
+``  <option value="option2_3">option2_3</option>``
+<br/>
+``  <option value="option2_4">option2_4</option>``
+<br/>
+``</select>``
+<br/>
+js&nbsp;&nbsp;&nbsp;=>&nbsp;&nbsp;&nbsp;selectMultiple: []
+<br/>
+<br/>
+
+
+
+<style>
+.code.html {
+  background-color:f8f8fa
+  border-radius:5px
+}
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,84 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <table>
+      <tr class="header">
+        <td>html</td>
+        <td></td>
+        <td>variable value</td>
+        <td>variable name</td>
+      </tr>
+      <tr>
+        <td><label>Type any text : </label><input type="text" v-model="myText"/></td>
+        <td>=></td>
+        <td>{{myText}}</td>
+        <td>myText</td>
+      </tr>
+      <tr>
+        <td><label for="myCheckbox">Check the checkbox : </label><input type="checkbox" v-model="myCheckbox"/></td>
+        <td>=></td>
+        <td>{{myCheckbox}}</td>
+        <td>myCheckbox</td>
+      </tr>
+      <tr>
+        <td><label for="myDate">Choose a date : </label><input type="date" v-model="myDate"/></td>
+        <td>=></td>
+        <td>{{myDate}}</td>
+        <td>myDate</td>
+      </tr>
+      <tr>
+        <td>
+          <label>Tick a radio button : </label>
+          <input type="radio" name="myRBGroup" id="rb1" v-model="myRB" value="RB1"/><label for="rb1">Radio button 1</label>
+          <input type="radio" name="myRBGroup" id="rb2" v-model="myRB" value="RB2"/><label for="rb2">Radio button 2</label>
+        </td>
+        <td>=></td>
+        <td>{{myRB}}</td>
+        <td>myRB</td>
+      </tr>
+      <tr>
+        <td>
+          <label>Select one option for select number 1 : </label>
+          <select v-model="selectSingle">
+            <option value="option1_1">option1_1</option>
+            <option value="option1_2">option1_2</option>
+            <option value="option1_3">option1_3</option>
+            <option value="option1_4">option1_4</option>
+          </select>
+        </td>
+        <td>=></td>
+        <td>{{selectSingle}}</td>
+        <td>selectSingle</td>
+      </tr>
+      <tr>
+        <td>
+          <label>Select one or several options for select number 2 : </label>
+          <select multiple v-model="selectMultiple">
+            <option value="option2_1">option2_1</option>
+            <option value="option2_2">option2_2</option>
+            <option value="option2_3">option2_3</option>
+            <option value="option2_4">option2_4</option>
+          </select>
+        </td>
+        <td>=></td>
+        <td>{{selectMultiple}}</td>
+        <td>selectMultiple</td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      myText: '',
+      myCheckbox: false,
+      myDate: null,
+      myRB: null,
+      selectSingle: null,
+      selectMultiple: []
+    }
   }
 }
 </script>
@@ -24,5 +91,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+table {
+  border:none;
+  margin-left:auto;
+  margin-right:auto;
+}
+
+.header {
+  font-weight:bold;
 }
 </style>
